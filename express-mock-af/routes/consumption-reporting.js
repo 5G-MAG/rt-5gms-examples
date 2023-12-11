4efad623e74a71fae150de853dafa614b809ca09
Daniel Silhavy <daniel.silhavy@fokus.fraunhofer.de>
2023-12-11 13:50:59 +0100

    Fix/consumption reporting changes (#7)
    * Minor corrections to consumption reporting endpoint
    
    * Increase version number
    
    * Add consumption reporting examples
    
    * Increase version number to 1.1.0

--- express-mock-af/routes/consumption-reporting.js
+++ express-mock-af/routes/consumption-reporting.js
@@ -1,14 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var querystring = require('querystring');
-var querystring = require('body-parser');
-
-
-router.post('/:provisisioningSessionId', function(req, res, next) {
-  const provisisioningSessionId = req.params.provisisioningSessionId;
-  //res.send("respond a resource for consumption-reporting with post for provisisioningSessionId:" + provisisioningSessionId);
-  
+router.post('/:provisioningSessionId', function(req, res, next) {
   res.send(204);
 });
 
--- express-mock-af/routes/m8.js
+++ express-mock-af/routes/m8.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 const m8 = {
-    m5BaseUrl: 'http://10.147.67.179:3003/3gpp-m5/v2/',
+    //m5BaseUrl: 'http://192.168.178.78:3003/3gpp-m5/v2/',
+    m5BaseUrl: 'http://10.147.67.10:3003/3gpp-m5/v2/',
     serviceList: [
         {
             provisioningSessionId: 1,
